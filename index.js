5c1dba2b6426663b6e288885e6e55dc0626339fa
Shawn Huang Fernandes <shawn.huang.fernandes@gmail.com>
2019-11-18 13:33:40 -0800

    Done.

--- index.js
+++ index.js
@@ -8,4 +8,29 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+
+function greet(timeString)
+{
+  let amOrPm = parseInt(timeString.split(":")[0]);
+
+  if (amOrPm < 12)
+  {
+    return "Good Morning";
+  }
+  else if (amOrPm <= 17)
+  {
+    return "Good Afternoon";
+  }
+  else
+  {
+    return "Good Evening";
+  }
+}
+
+function displayMessage(content)
+{
+  let addTo = document.getElementById('greeting');
+  addTo.innerText = content;
+  // addTo.appendChild(message);
+}
 /* Write your implementation of displayMessage() */
